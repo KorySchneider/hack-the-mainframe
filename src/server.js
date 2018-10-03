b25ed4108995e13538c8d351831d43d53a411ff4
Kory Schneider <kory.schneider@gmail.com>
2018-10-03 15:28:12 -0700

    Display contents of server

--- src/server.js
+++ src/server.js
@@ -1,11 +1,46 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const fs = require('fs');
+const shell = require('shelljs');
 
 app.get('/', (req, res) => {
+  fs.readFile(`${__filename}`, 'utf-8', (err, contents) => {
 
-  res.send('Welcome to the website.');
+    res.send(`
+    <html>
+      <textarea name='server' autofocus>
+        ${escape(contents)}
+      </textarea>
 
+      <br/>
+
+      <button>Reload server</button>
+
+      <script>
+        window.onload = () => {
+          let textarea = document.querySelector('textarea');
+          textarea.value = unescape(textarea.value).trim();
+        }
+      </script>
+
+      <style>
+        html, body {
+          font-family: monospace;
+          text-align: center;
+        }
+        textarea {
+          width: 700;
+          height: 800;
+        }
+      </style>
+    </html>
+    `);
+
+  });
 });
 
-app.listen(port);
+app.post('/', (req, res) => {
+  shell.exec(`./restart.sh`);
+});
+
+app.listen(8080);
